f1ed685c4372805d7916f99d92cab383b474c2c0
Azul <mail@azulinho.com>
2015-09-29 16:56:04 +0100

    added 1.1.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Bookshelf',
-      version='1.1.4',
+      version='1.1.5',
       description='Bookshelf of Fabric functions',
       author='ClusterHQ',
       author_email='nospam@clusterhq.com',
