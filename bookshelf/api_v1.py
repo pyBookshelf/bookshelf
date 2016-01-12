e0aed0ef1d376874c96bf319420eb4a0e22b674e
James Westby <james.westby@clusterhq.com>
2016-01-12 17:13:14 +0000

    Allow disabling rDNS lookup.
    This can be very costly when it fails because it takes a few
    seconds to timeout. This happens whenever a new channel (not connection)
    is created, which can really add up if that happens a lot (e.g. with fabric).

--- bookshelf/api_v1.py
+++ bookshelf/api_v1.py
@@ -151,6 +151,18 @@ def arch():
     return result
 
 
+def disable_openssh_rdns(distribution):
+    log_green('Disabling openssh reverse dns lookups')
+    openssh_config_file = '/etc/ssh/sshd_config'
+    dns_config = 'UseDNS no'
+    if not file_contains(openssh_config_file, dns_config, use_sudo=True):
+        file_append(openssh_config_file, dns_config, use_sudo=True)
+        service_name = 'sshd'
+        if 'ubuntu' in distribution:
+            service_name = 'ssh'
+        sudo('service {} reload'.format(service_name))
+
+
 def cache_docker_image_locally(docker_image):
     # download docker images to speed up provisioning
     log_green('pulling docker image %s locally' % docker_image)
