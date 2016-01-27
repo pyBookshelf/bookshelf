c9a9b9f22b7b843e2d2520ffc0192eed8009af95
Azul <mail@azulinho.com>
2016-01-27 11:16:30 +0000

    adding logging helpers

---
+++ bookshelf/api_v2/logging_helpers.py
@@ -0,0 +1,13 @@
+from fabric.colors import green, yellow, red
+
+
+def log_green(msg):
+    print(green(msg))
+
+
+def log_yellow(msg):
+    print(yellow(msg))
+
+
+def log_red(msg):
+    print(red(msg))
