d5f185d5b273d2682d750daf28238fe7bd3e702d
Azul <mail@azulinho.com>
2016-01-28 10:24:56 +0000

    release 2.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 
 setup(name='Bookshelf',
-      version='2.0.0',
+      version='2.0.1',
       description='Bookshelf of Fabric functions',
       author='ClusterHQ',
       author_email='nospam@clusterhq.com',
