2f22020a03f4d8cb5c127c0e67883d760a9f6ce2
Marcus Ewert <sarum90@gmail.com>
2016-02-09 18:30:38 -0800

    Revert "[FLOC-4156] Tests for v3 of the API."

--- setup.py
+++ setup.py
@@ -4,18 +4,15 @@ except ImportError:
     from distutils.core import setup
 
 
-setup(
-    name='Bookshelf',
-    version='3.0.0',
-    description='Bookshelf of Fabric functions',
-    author='ClusterHQ',
-    author_email='nospam@clusterhq.com',
-    url='https://www.github.com/ClusterHQ/Bookshelf/',
-    packages=['bookshelf', 'bookshelf.api_v2', 'bookshelf.api_v3',
-              'bookshelf.tests', 'bookshelf.tests.api_v2',
-              'bookshelf.tests.api_v3', ],
-    install_requires=['cuisine', 'fabric', 'pyrax', 'boto',
-                      'google-api-python-client', 'oauth2client',
-                      'zope.interface', 'flufl.enum', 'pyrsistent'],
-    license='Apache License, Version 2.0',
-)
+setup(name='Bookshelf',
+      version='2.0.4',
+      description='Bookshelf of Fabric functions',
+      author='ClusterHQ',
+      author_email='nospam@clusterhq.com',
+      url='https://www.github.com/ClusterHQ/Bookshelf/',
+      packages=['bookshelf', 'bookshelf.api_v2', 'bookshelf.tests',
+                'bookshelf.tests.api_v2', ],
+      install_requires=['cuisine', 'fabric', 'pyrax', 'boto',
+                        'google-api-python-client', 'oauth2client'],
+      license='Apache License, Version 2.0',
+     )
