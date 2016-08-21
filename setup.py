a6770678e735de95b194f6e6989223970db5f654
Azul <mail@azulinho.com>
2016-08-21 23:48:39 +0100

    updated version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except ImportError:
 
 setup(
     name='pyBookshelf',
-    version='3.0.1',
+    version='3.0.2',
     description='Bookshelf of Fabric functions',
     author='Jorge Costa',
     author_email='pybookshelf@azulinho.com',
